7f2684180d4b9a8010e7ea0cb6cc4547246717a3
Mohamed <DrKrar@users.noreply.github.com>
2017-10-03 02:34:48 +0200

    add @async readline()

--- test/testWrite.jl
+++ test/testWrite.jl
@@ -1,4 +1,9 @@
 info("""write(STDIN,"5")""")
 write(STDIN,"5\n")
+@async( global x = readline())
+print(x)
+
 info("""write(STDIN.buffer,"5")""")
 write(STDIN.buffer,"5\n")
+@async( global y = readline())
+print(y)
