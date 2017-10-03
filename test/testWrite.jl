1a1f7f5cec73ddf37e2ff5a6f89eabc19d656308
Mohamed <DrKrar@users.noreply.github.com>
2017-10-03 02:28:40 +0200

    Update testWrite.jl
    [ci skip]

--- test/testWrite.jl
+++ test/testWrite.jl
@@ -1,4 +1,4 @@
-info("""write(STDIN,"5\n")""")
+info("""write(STDIN,"5")""")
 write(STDIN,"5\n")
-info("""write(STDIN.buffer,"5\n")""")
+info("""write(STDIN.buffer,"5")""")
 write(STDIN.buffer,"5\n")
