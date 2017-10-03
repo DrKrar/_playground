4f9f243d13576d7dc8047ef76a7d59889dd5dccd
Mohamed <DrKrar@users.noreply.github.com>
2017-10-03 03:44:06 +0200

    using  Compat.Sys

--- test/testWrite.jl
+++ test/testWrite.jl
@@ -1,11 +1,11 @@
-using  Compat
+using  Compat.Sys
 
 info("""write(STDIN,"5")""")
 write(STDIN,"5\n")
 x = readline()
 print("x = ", x)
 
-if  @compat(!Sys.isapple())
+if  !Sys.isapple()
   info("""write(STDIN.buffer,"5")""")
   write(STDIN.buffer,"5\n")
   y = readline()
