fab696871de438c4a290c7ccd01c0a0edae6e00c
Mohamed <DrKrar@users.noreply.github.com>
2017-10-03 03:23:12 +0200

    using Compat
    [ci skip]

--- test/testWrite.jl
+++ test/testWrite.jl
@@ -1,10 +1,11 @@
+using  Compat
 
 info("""write(STDIN,"5")""")
 write(STDIN,"5\n")
 x = readline()
 print("x = ", x)
 
-if !Sys.isapple()
+if  @compat(!Sys.isapple())
   info("""write(STDIN.buffer,"5")""")
   write(STDIN.buffer,"5\n")
   y = readline()
